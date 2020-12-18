59b6eddb82d42e513ffef0a5508cf486586ab2b2
DaangamesDG <dwklarenbeek@gmail.com>
2020-12-18 20:27:37 +0100

    added main bot status

--- src/events/ClientEvents/ready/ready.ts
+++ src/events/ClientEvents/ready/ready.ts
@@ -11,9 +11,22 @@ export default class ReadyEvent extends BaseEvent {
     client.music.init(client.user.id);
     console.log(`${client.user.tag} has logged in.`);
 
-    this.devStatus(client);
+    client.user.id === "750232563779633153" ? this.devStatus(client) : this.status(client);
     
     client.guilds.cache.forEach(guild => this.loadConfig(client, guild));
+
+    if (client.user.username === 'Stereo') {
+      setInterval(async function() {
+        let channelId = '751459649538228256';
+        let guild = client.guilds.cache.get(`743145077206941747`);
+        let channel = guild.channels.cache.get(channelId);
+        let count = client.guilds.cache.size;
+  
+        if (!channel) return console.log('no channel found');
+        await channel.edit({ name: `🎊 Server Count: ${count}`}, `10 minutes passed. Changing server count`);
+  
+      }, 300000);
+    }
   }
 
   async loadConfig(client: DiscordClient, guild: Guild) {
@@ -36,7 +49,8 @@ export default class ReadyEvent extends BaseEvent {
   }
 
   async status(client: DiscordClient) {
-    await client.user.setActivity('with v3 👀', { type: "PLAYING" });
-    await client.user.setStatus('idle');
+    let activities = ['+help', 'with my dj set', 'Mention me to see the prefix', `in ${client.guilds.cache.size} servers!`], i = 0;
+    setInterval(() => client.user.setActivity(activities[i++ % activities.length] + ' | Stereo Music', { type: 'PLAYING' }), 15000);
+    await client.user.setStatus('online');
   }
 }
